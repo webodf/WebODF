7649813143bb15a42502509bcfdb7923b2d1d64c
Jos van den Oever <jos@vandenoever.info>
2010-10-19 15:45:54 +0200

    Work on the website.

--- www/clean.sh
+++ www/clean.sh
@@ -8,4 +8,5 @@ if xmlstarlet val -d /usr/share/xml/xhtml/1.0/xhtml1-strict.dtd $1; then
 	echo '<?xml version="1.0" encoding="utf-8" ?>' > $1
 	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' >> $1
 	xmlstarlet c14n tmp >> $1
+	rm tmp
 fi
