b5550efb98dbf95b9a011e885999efbfad3c80f0
Jos van den Oever <jos@vandenoever.info>
2010-11-27 15:07:06 +0100

    Simplify test.

--- webodf/tests/core/RuntimeTests.js
+++ webodf/tests/core/RuntimeTests.js
@@ -22,46 +22,37 @@ core.RuntimeTests = function RuntimeTests(runner) {
      * Test writing a binary file and reading it back.
      */
     function testWrite(callback) {
-        var content1 = "", content2 = "", i, max = 1024, filename, clean;
+        var content = "", i, max = 1024, filename, clean;
         for (i = 0; i < max; i += 1) {
-            content1 += String.fromCharCode(i);
-            content2 += String.fromCharCode(max - i - 1);
+            content += String.fromCharCode(i);
         }
         filename = "tmp" + Math.random();
-        runtime.log(filename);
-        // read to see if a file is there already
-        runtime.readFile(filename, "binary", function (err, data) {
-            if (data === content1) {
-                content1 = content2;
-            }
-            clean = "";
-            for (i = 0; i < max; i += 1) {
-                clean += String.fromCharCode(content1.charCodeAt(i) & 0xff);
-            }
-            // now content1 has content different from what is on the server
-            runtime.writeFile(filename, content1, "binary", function (err) {
-                runtime.log("wrote " + filename + " " + err);
+        clean = "";
+        for (i = 0; i < max; i += 1) {
+            clean += String.fromCharCode(content.charCodeAt(i) & 0xff);
+        }
+        // now content has content different from what is on the server
+        runtime.writeFile(filename, content, "binary", function (err) {
+            t.err = err;
+            r.shouldBeNull(t, "t.err");
+            runtime.readFile(filename, "binary", function (err, data) {
                 t.err = err;
                 r.shouldBeNull(t, "t.err");
-                runtime.readFile(filename, "binary", function (err, data) {
-                    t.err = err;
-                    r.shouldBeNull(t, "t.err");
-                    t.data = data;
-                    t.clean = clean;
-                    r.shouldBe(t, "t.data.length", "t.clean.length");
-                    for (i = 0; i < max && data[i] === clean[i]; i += 1) {}
-                    if (i !== max) {
-                        runtime.log(data.charCodeAt(i) + " vs " +
-                            clean.charCodeAt(i));
-                    }
-                    t.i = i;
-                    t.max = max;
-                    r.shouldBe(t, "t.i", "t.max");
-                    r.shouldBe(t, "t.data", "t.clean");
-                    // cleanup
-                    runtime.deleteFile(filename, function (err) {
-                        callback();
-                    });
+                t.data = data;
+                t.clean = clean;
+                r.shouldBe(t, "t.data.length", "t.clean.length");
+                for (i = 0; i < max && data[i] === clean[i]; i += 1) {}
+                if (i !== max) {
+                    runtime.log(data.charCodeAt(i) + " vs " +
+                        clean.charCodeAt(i));
+                }
+                t.i = i;
+                t.max = max;
+                r.shouldBe(t, "t.i", "t.max");
+                r.shouldBe(t, "t.data", "t.clean");
+                // cleanup
+                runtime.deleteFile(filename, function (err) {
+                    callback();
                 });
             });
         });
