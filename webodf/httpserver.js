c021fcac1064938eaeab9b0120337a6af7fe861d
Jos van den Oever <jos@vandenoever.info>
2011-01-10 09:21:38 +0100

    Change default port.

--- webodf/httpserver.js
+++ webodf/httpserver.js
@@ -108,6 +108,6 @@ http.createServer(function (request, response) {
             response.end();
         }
     });
-}).listen(8123);
+}).listen(8124);
 
-console.log('Server running at http://127.0.0.1:8123/');
+console.log('Server running at http://127.0.0.1:8124/');
