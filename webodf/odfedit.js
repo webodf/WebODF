f3511d5433a1b426e9b5b4aa3d0ed7fe7deb4bee
Jos van den Oever <jos@vandenoever.info>
2011-04-28 15:14:31 +0200

    Nicer toolbar, still needs to be hooked up.

--- webodf/odfedit.js
+++ webodf/odfedit.js
@@ -1,6 +1,9 @@
 /*global runtime document odf window Ext*/
 var odfcanvas;
 
+/**
+ * @return {undefined}
+ */
 function fixExtJSCSS() {
     // look through all stylesheets to change the selector
     // ".x-viewport, .x-viewport body"
@@ -24,24 +27,29 @@ function fixExtJSCSS() {
         }
     }
 }
-
+/**
+ * @return {undefined}
+ */
 function updateStyleComboBox() {
     var paragraphStylesBox = document.getElementById("paragraphStyleBox");
 }
-
+/**
+ * @param {!Element} odfelement
+ * @return {undefined}
+ */
 function initCanvas(odfelement) {
     runtime.loadClass("odf.OdfCanvas");
     // if the url has a fragment (#...), try to load the file it represents
     var location = String(document.location),
         pos = location.indexOf('#');
-    odfelement.style.overflow = 'auto';
+//    odfelement.style.overflow = 'auto';
+    odfelement.style.height = '100%';
     odfcanvas = new odf.OdfCanvas(odfelement);
     if (pos === -1 || !window) {
         return;
     }
     location = location.substr(pos + 1);
     odfcanvas.onstatereadychange = function () {
-//        odfelement.style.height = odfcanvas.getHeight();
         /*
         updateStyleComboBox();
         odfcanvas.save(function (err) {
@@ -51,6 +59,9 @@ function initCanvas(odfelement) {
     };
     odfcanvas.load(location);
 }
+/**
+ * @return {undefined}
+ */
 function save() {
     odfcanvas.odfContainer().save(function (err) {
         if (err) {
@@ -58,35 +69,186 @@ function save() {
         }
     });
 }
+Ext.ODFEditor = Ext.extend(Ext.BoxComponent, {
+    buttonTips : {
+        bold : {
+            title: 'Bold (Ctrl+B)',
+            text: 'Make the selected text bold.',
+            cls: 'x-html-editor-tip'
+        },
+        italic : {
+            title: 'Italic (Ctrl+I)',
+            text: 'Make the selected text italic.',
+            cls: 'x-html-editor-tip'
+        },
+        underline : {
+            title: 'Underline (Ctrl+U)',
+            text: 'Underline the selected text.',
+            cls: 'x-html-editor-tip'
+        },
+        increasefontsize : {
+            title: 'Grow Text',
+            text: 'Increase the font size.',
+            cls: 'x-html-editor-tip'
+        },
+        decreasefontsize : {
+            title: 'Shrink Text',
+            text: 'Decrease the font size.',
+            cls: 'x-html-editor-tip'
+        },
+        backcolor : {
+            title: 'Text Highlight Color',
+            text: 'Change the background color of the selected text.',
+            cls: 'x-html-editor-tip'
+        },
+        forecolor : {
+            title: 'Font Color',
+            text: 'Change the color of the selected text.',
+            cls: 'x-html-editor-tip'
+        },
+        justifyleft : {
+            title: 'Align Text Left',
+            text: 'Align text to the left.',
+            cls: 'x-html-editor-tip'
+        },
+        justifycenter : {
+            title: 'Center Text',
+            text: 'Center text in the editor.',
+            cls: 'x-html-editor-tip'
+        },
+        justifyright : {
+            title: 'Align Text Right',
+            text: 'Align text to the right.',
+            cls: 'x-html-editor-tip'
+        },
+        insertunorderedlist : {
+            title: 'Bullet List',
+            text: 'Start a bulleted list.',
+            cls: 'x-html-editor-tip'
+        },
+        insertorderedlist : {
+            title: 'Numbered List',
+            text: 'Start a numbered list.',
+            cls: 'x-html-editor-tip'
+        },
+        createlink : {
+            title: 'Hyperlink',
+            text: 'Make the selected text a hyperlink.',
+            cls: 'x-html-editor-tip'
+        },
+        sourceedit : {
+            title: 'Source Edit',
+            text: 'Switch to source editing mode.',
+            cls: 'x-html-editor-tip'
+        },
+        save : {
+            title: 'Save (Ctrl+S)',
+            text: 'Save the document.',
+            cls: 'x-html-editor-tip'
+        }
+    }
+});
+
+
+var ODFEditor = Ext.extend(Ext.Panel, {
+    initComponent: function () {
+        var me = this;
+        function buttonHandler(button, event) {
+        }
+        this.defaults = {
+        };
+        this.initialConfig = Ext.apply({
+        }, this.initialConfig);
+        this.items = [{
+            xtype: 'box',
+            id: 'canvas',
+            autoEl: {
+                tag: 'div',
+                frameBorder: 0,
+                style: {
+                    border: '0 none'
+                }
+            },
+            autoScroll: true,
+            scroll: true
+        }];
+        this.tbar = {
+            xtype: 'toolbar',
+            items: [{
+                xtype: 'button',
+                icon: 'extjs/examples/shared/icons/save.gif',
+                handler: buttonHandler,
+                cls: 'x-btn-icon'
+            }, {
+                tag: 'select',
+                //html: this.createFontOptions()
+                cls: 'x-font-select'
+            }, {
+                xtype: 'buttongroup',
+                cls: 'x-html-editor-tb',
+                frame: false,
+                items: [{
+                    xtype: 'button',
+                    iconCls: 'x-edit-bold',
+                    cls: 'x-btn-icon'
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-edit-italic',
+                    cls: 'x-btn-icon'
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-edit-underline',
+                    cls: 'x-btn-icon'
+                }, {
+                    itemId: 'forecolor',
+                    cls: 'x-btn-icon',
+                    iconCls: 'x-edit-forecolor',
+                    menu: { xtype: 'colormenu' }
+                }, {
+                    itemId: 'backcolor',
+                    cls: 'x-btn-icon',
+                    iconCls: 'x-edit-backcolor',
+                    menu: { xtype: 'colormenu' }
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-edit-justifyleft',
+                    cls: 'x-btn-icon'
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-edit-justifycenter',
+                    cls: 'x-btn-icon'
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-edit-justifyright',
+                    cls: 'x-btn-icon'
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-edit-insertorderedlist',
+                    cls: 'x-btn-icon'
+                }, {
+                    xtype: 'button',
+                    iconCls: 'x-edit-insertunorderedlist',
+                    cls: 'x-btn-icon'
+                }]
+            }]
+        };
+        ODFEditor.superclass.initComponent.call(this);
+    }
+});
 Ext.onReady(function () {
-    var toolbar, canvas, viewport;
+    var canvas, viewport;
 
     Ext.QuickTips.init();
 
-    toolbar = new Ext.TabPanel({
-        region: 'north'
-    });
-
-    canvas = new Ext.BoxComponent({
-        closable: true,
-        autoEl: {
-            tag: 'div',
-            name: 'canvas',
-            frameBorder: 0,
-            style: {
-                border: '0 none'
-            }
-        },
-        region: 'center',
-        autoScroll: true,
-        scroll: true
+    canvas = new ODFEditor({
+        region: 'center'
     });
 
     viewport = new Ext.Viewport({
         layout: 'border',
-        items: [ toolbar, canvas ]
+        items: [ canvas ]
     });
 
     fixExtJSCSS();
-    initCanvas(canvas.getEl().dom);
+    initCanvas(Ext.getCmp('canvas').el.dom);
 });
