62a3b09ba9856631df4d206043111dbffb8bf89c
Jos van den Oever <jos@vandenoever.info>
2012-06-11 16:57:21 +0200

    Add menu items to visit the webpage or report bugs.

--- programs/android/src/org/webodf/WebODFActivity.java
+++ programs/android/src/org/webodf/WebODFActivity.java
@@ -1,6 +1,11 @@
 package org.webodf;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.phonegap.DroidGap;
 
@@ -30,4 +35,49 @@ public class WebODFActivity extends DroidGap {
 		sendJavascript("invokeString = '" + escapedPath + "';");
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.report_bug:
+			reportIssue();
+			return true;
+		case R.id.about:
+			about();
+			return true;
+		case R.id.visit_website:
+			visitWebODFWebsite();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void visitWebODFWebsite() {
+		String url = "http://webodf.org/";
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		startActivity(i);
+	}
+
+	private void about() {
+		String url = "http://webodf.org/about/";
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		startActivity(i);
+	}
+
+	private void reportIssue() {
+		String url = "http://webodf.org/redmine/projects/webodf/issues";
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		startActivity(i);
+	}
 }
