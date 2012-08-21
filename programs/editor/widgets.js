8203e783383c6e2ca8a607f11712fcb70967af90
Aditya Bhatt <aditya@adityabhatt.org>
2012-08-21 18:55:25 +0530

    add SimpleStyles widget to app

--- programs/editor/widgets.js
+++ programs/editor/widgets.js
@@ -1,5 +1,7 @@
 function loadWidgets(documentObject) {
-	var dojoWin, toolbar;
+	var dojoWin, toolbar,
+        ToolbarSeparator;
+
 	require(["dojo/_base/window"], function (win) {
 		dojoWin = win;
 	});
@@ -7,6 +9,14 @@ function loadWidgets(documentObject) {
     // Toolbar
     require(["dijit/Toolbar"], function(Toolbar) {
         toolbar = new Toolbar({}, "toolbar");
+
+        // Simple Style Selector [B, I, U, S]
+        require(["widgets/simpleStyles.js"], function () {
+            var styles = new widgets.SimpleStyles(documentObject, function (widget) {
+                widget.placeAt(toolbar);
+                widget.startup();
+            });
+        });
 
         // Paragraph Style Selector
         require(["widgets/paragraphStyles.js"], function () {
