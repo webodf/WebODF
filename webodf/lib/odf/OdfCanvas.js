f9c82f9e5a93f208735ac379e7ab993df4fcb915
Tobias Hintze <th@kogmbh.com>
2012-09-17 16:52:26 +0200

    workaround recent JSLint pain (typeof foo !== "undefined")
    Conflicts:
    
    	webodf/lib/odf/OdfCanvas.js

--- webodf/lib/odf/OdfCanvas.js
+++ webodf/lib/odf/OdfCanvas.js
@@ -805,7 +805,7 @@ odf.OdfCanvas = (function () {
         var head = document.getElementsByTagName('head')[0],
             style,
             href;
-        if (typeof (webodf_css) !== 'undefined') {
+        if (String(typeof webodf_css) !== "undefined") {
             style = document.createElementNS(head.namespaceURI, 'style');
             style.setAttribute('media', 'screen, print, handheld, projection');
             style.appendChild(document.createTextNode(webodf_css));
