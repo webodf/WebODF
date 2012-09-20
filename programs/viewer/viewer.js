ff98c97f39065963e213de9181bb4d80b1ff4e86
Aditya Bhatt <aditya@adityabhatt.org>
2012-09-20 21:31:54 +0530

    update viewer

--- programs/viewer/viewer.js
+++ programs/viewer/viewer.js
@@ -4,11 +4,35 @@ function isFullScreen() {
     return  document.mozFullScreen || document.webkitIsFullScreen;
 }
 
+function selectScaleOption(value) {
+  var options = document.getElementById('scaleSelect').options;
+  var predefinedValueFound = false;
+  for (var i = 0; i < options.length; i++) {
+    var option = options[i];
+    if (option.value != value) {
+      option.selected = false;
+      continue;
+    }
+    option.selected = true;
+    predefinedValueFound = true;
+  }
+  return predefinedValueFound;
+}
+
+
+var kCssUnits = 96.0 / 72.0;
+var kScrollbarPadding = 40;
+var kMinScale = 0.25;
+var kMaxScale = 4.0;
+var kDefaultScaleDelta = 1.1;
+var kDefaultScale = 'auto';
+
 var Viewer = {
     url: null,
     filename: null,
     odfCanvas: null,
     element: null,
+    initialized: false,
 
     initialize: function() {
         // If the URL has a fragment (#...), try to load the file it represents
@@ -31,6 +55,8 @@ var Viewer = {
 
         if(! (document.cancelFullScreen || document.mozCancelFullScreen || document.webkitCancelFullScreen) )
             document.getElementById('fullscreen').style.visibility = 'hidden';
+
+        this.initialized = true;
     },
 
     download: function() {
@@ -65,16 +91,66 @@ var Viewer = {
         return this.odfCanvas.getZoomLevel();
     },
 
+    setScale: function (val, resetAutoSettings, noScroll) {
+        if (val == this.zoomLevel() )
+          return;
+
+        this.odfCanvas.setZoomLevel(val);
+        /*var event = document.createEvent('UIEvents');
+        event.initUIEvent('scalechange', false, false, window, 0);
+        event.scale = val;
+        event.resetAutoSettings = resetAutoSettings;
+        window.dispatchEvent(event);*/
+    },
+
+    parseScale: function (value, resetAutoSettings, noScroll) {
+        if ('custom' == value)
+          return;
+
+        var scale = parseFloat(value);
+        if (scale) {
+            this.setScale(scale, true, noScroll);
+            return;
+        }
+
+        var container = this.element.parentNode;
+
+        var pageWidthScale = (container.clientWidth - kScrollbarPadding) /
+                              this.element.clientWidth * this.zoomLevel() / kCssUnits;
+        /*console.log('value ' + value);
+        console.log('zoomlevel ' + this.zoomLevel());
+        console.log('clientWidth ' + this.element.clientWidth);
+        console.log('pageWidthScale ' + pageWidthScale);*/
+        switch (value) {
+          case 'page-actual':
+            scale = 1;
+            break;
+          case 'page-width':
+            scale = pageWidthScale;
+            this.odfCanvas.fitToWidth(document.body.clientWidth - kScrollbarPadding);
+            break;
+          case 'auto':
+            scale = Math.min(1.0, pageWidthScale);
+            break;
+        }
+        //console.log('scale ' + scale);
+        //this.setScale(scale, resetAutoSettings, noScroll);
+
+        selectScaleOption(value);
+    },
+
     zoomOut: function() {
         // 10 % decrement
-        var newLevel = this.zoomLevel() * 0.9;
-        this.odfCanvas.setZoomLevel(newLevel);
+        var newScale = (this.zoomLevel() / kDefaultScaleDelta).toFixed(2);
+        newScale = Math.max(kMinScale, newScale);
+        this.parseScale(newScale, true);
     },
 
     zoomIn: function() {
         // 10 % increment
-        var newLevel = this.zoomLevel() * 1.1;
-        this.odfCanvas.setZoomLevel(newLevel);
+        var newScale = (this.zoomLevel() * kDefaultScaleDelta).toFixed(2);
+        newScale = Math.min(kMaxScale, newScale);
+        this.parseScale(newScale, true);
     }
 };
 
@@ -93,4 +169,11 @@ window.onload = function() {
     document.getElementById('zoomIn').addEventListener('click', function() {
         Viewer.zoomIn();
     });
+    window.addEventListener('resize', function webViewerResize(evt) {
+        if (Viewer.initialized &&
+          (document.getElementById('pageWidthOption').selected ||
+          document.getElementById('pageAutoOption').selected))
+          Viewer.parseScale(document.getElementById('scaleSelect').value);
+    });
+
 };
