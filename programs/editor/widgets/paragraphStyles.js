6cbb94df7433a8bd103dd9c61b4d1f768082d1b1
Aditya Bhatt <aditya@adityabhatt.org>
2012-10-03 19:46:41 +0530

    bind paragraph selector

--- programs/editor/widgets/paragraphStyles.js
+++ programs/editor/widgets/paragraphStyles.js
@@ -1,17 +1,21 @@
 widgets.ParagraphStyles = (function () {
 
+    var textns = "urn:oasis:names:tc:opendocument:xmlns:text:1.0";
+
     function makeWidget(documentObject, callback) {
         require(["dijit/form/Select"], function (Select) {
             var i,
                 widget,
                 selectionList = [],
-                availableStyles = documentObject.odfCanvas.getFormatting().getAvailableParagraphStyles();
+                availableStyles = documentObject.odfCanvas.getFormatting().getAvailableParagraphStyles(),
+                currentParagraph = null;
 
             for (i = 0; i < availableStyles.length; i += 1) {
                 selectionList.push({
                     label: availableStyles[i].displayName,
                     value: availableStyles[i].name
                 });
+                console.log(availableStyles[i].name);
             }
 
             widget = new Select({
@@ -23,9 +27,20 @@ widgets.ParagraphStyles = (function () {
                 }
             });
 
-            documentObject.odfCanvas.addListener("selectionchange", function (element, selection) {
-                console.log(documentObject.odfCanvas.getFormatting().getParagraphStyles(selection));
+            documentObject.odfCanvas.addListener("click", function (event) {
+                var e = event.target;
+                while (e && !((e.localName === "p" || e.localName === "h") && e.namespaceURI === textns)) {
+                    e = e.parentNode;
+                }
+                currentParagraph = e;
+                widget.set("value", e.getAttributeNS(textns, 'style-name'));
             });
+
+            widget.onChange = function(value) {
+                if(currentParagraph) {
+                    currentParagraph.setAttributeNS(textns, 'style-name', value);
+                }
+            }
 
             return callback(widget);
         });
