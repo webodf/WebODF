917c85e47f6a97bb712fded2d85bffa18e794752
Friedrich W. H. Kossebau <friedrich@kogmbh.com>
2012-12-22 08:54:30 +0100

    Restore programs/editor working without middleware

--- programs/editor/getin.js
+++ programs/editor/getin.js
@@ -1,17 +1,27 @@
 
-function letIn() {
+function on_body_load() {
     var net = runtime.getNetwork(),
         accumulated_waiting_time = 0,
         userId;
 
+    function bootEditor(documentPath, userId, sessionid) {
+        require({ }, ["webodf/editor"],
+            function(editor) {
+                editor.boot(documentPath, userId, sessionid);
+            }
+        );
+    }
+
+    function bootEditorWithoutNetwork() {
+        document.getElementById("mainContainer").style.display="";
+        bootEditor("welcome.odt");
+    }
+
     function enterSession(sessionid) {
         document.getElementById("sessionListContainer").style.display="none";
         document.getElementById("mainContainer").style.display="";
-        require({ }, ["webodf/editor"],
-            function(editor) {
-                editor.boot("/session/"+sessionid+"/genesis", userId, sessionid);
-            }
-        );
+
+        bootEditor("/session/"+sessionid+"/genesis", userId, sessionid);
     }
 
     function showSessions() {
@@ -37,22 +47,34 @@ function letIn() {
         alert("Login failed: " + result);
     }
 
+    function tryToGetIn() {
+         net.login(document.loginForm['login'].value, document.loginForm['password'].value, loginSuccess, loginFail);
+
+        // block the submit button, we already dealt with the input
+        return false;
+    }
+
     function later_cb() {
         if (net.networkStatus === "unavailable") {
             runtime.log("connection to server unavailable.");
+            bootEditorWithoutNetwork();
             return;
         }
         if (net.networkStatus !== "ready") {
             if (accumulated_waiting_time > 8000) {
                 // game over
                 runtime.log("connection to server timed out.");
+                bootEditorWithoutNetwork();
                 return;
             }
             accumulated_waiting_time += 100;
             runtime.getWindow().setTimeout(later_cb, 100);
         } else {
             runtime.log("connection to collaboration server established.");
-            net.login(document.loginForm['login'].value, document.loginForm['password'].value, loginSuccess, loginFail);
+
+            document.loginForm['Submit'].onclick = tryToGetIn;
+            // show login view
+            document.getElementById("loginContainer").style.display="";
         }
     }
     later_cb();
