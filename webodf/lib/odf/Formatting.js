b1b8ce364fcd4283b7b293743379538d4e0e3259
Aditya Bhatt <aditya@adityabhatt.org>
2013-01-22 15:50:39 +0530

    Lint

--- webodf/lib/odf/Formatting.js
+++ webodf/lib/odf/Formatting.js
@@ -82,20 +82,18 @@ odf.Formatting = function Formatting() {
     function mergeRecursive(obj1, obj2) {
         var p;
         for (p in obj2) {
-            try {
-                // Property in destination object set; update its value.
-                if (obj2[p].constructor === Object) {
-                    obj1[p] = mergeRecursive(obj1[p], obj2[p]);
-                    
-                } else {
+            if (obj2.hasOwnProperty(p)) {
+                try {
+                    // Property in destination object set; update its value.
+                    if (obj2[p].constructor === Object) {
+                        obj1[p] = mergeRecursive(obj1[p], obj2[p]);
+                    } else {
+                        obj1[p] = obj2[p];
+                    }
+                } catch (e) {
+                    // Property in destination object not set; create it and set its value.
                     obj1[p] = obj2[p];
-                    
                 }
-                
-            } catch (e) {
-                // Property in destination object not set; create it and set its value.
-                obj1[p] = obj2[p];
-                
             }
         }
         return obj1;
