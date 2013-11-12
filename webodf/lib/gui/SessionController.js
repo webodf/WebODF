13cd41b0189e47425e61d8afb3ad9186944b43e8
Philip Peitsch <philip.peitsch@gmail.com>
2013-11-12 15:23:21 +1100

    Pasting data will replace the currently selected content

--- webodf/lib/gui/SessionController.js
+++ webodf/lib/gui/SessionController.js
@@ -790,6 +790,7 @@ gui.SessionController = (function () {
             }
 
             if (plainText) {
+                textManipulator.removeCurrentSelection();
                 session.enqueue(pasteHandler.paste(plainText));
                 cancelEvent(e);
             }
