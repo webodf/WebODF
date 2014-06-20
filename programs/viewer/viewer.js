0b6f070e094e212c40b641e3027d872e130017c0
Aditya Bhatt <aditya@bhatts.org>
2014-06-20 15:51:20 +0200

    Add original Mozilla licence notice to relevant viewer files.

--- programs/viewer/viewer.js
+++ programs/viewer/viewer.js
@@ -22,6 +22,26 @@
  * @source: https://github.com/kogmbh/WebODF/
  */
 
+/*
+ * This file is a derivative from a part of Mozilla's PDF.js project. The
+ * original license header follows.
+ */
+
+/* Copyright 2012 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*global document, window*/
 
 function Viewer(viewerPlugin) {
