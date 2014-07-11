webodf/WebODF

commit 3ae9e0c734c984588e563db747124f4c4b509382

Put OdfCanvas helper attributes "colspan", "rowspan" & "id" into the HTML document's native namespace
Prevents them leaking into the saved ODF files

webodf/lib/odf/OdfCanvas.js
@@ -450,9 +450,10 @@ odf.OdfCanvas = (function () {
     /**
      * Modify tables to support merged cells (col/row span)
      * @param {!Element} odffragment
+     * @param {!string} documentns
      * @return {undefined}
      */
-    function modifyTables(odffragment) {
+    function modifyTables(odffragment, documentns) {
         var i,
             tableCells,
             node;
@@ -465,11 +466,11 @@ odf.OdfCanvas = (function () {
             // If we have a cell which spans columns or rows,
             // then add col-span or row-span attributes.
             if (node.hasAttributeNS(tablens, "number-columns-spanned")) {
-                node.setAttribute("colspan",
+                node.setAttributeNS(documentns, "colspan",
                     node.getAttributeNS(tablens, "number-columns-spanned"));
             }
             if (node.hasAttributeNS(tablens, "number-rows-spanned")) {
-                node.setAttribute("rowspan",
+                node.setAttributeNS(documentns, "rowspan",
                     node.getAttributeNS(tablens, "number-rows-spanned"));
             }
         }
@@ -837,9 +838,10 @@ odf.OdfCanvas = (function () {
      * Load all the lists that are inside an odf element, and correct numbering.
      * @param {!Element} odffragment
      * @param {!StyleSheet} stylesheet
+     * @param {!string} documentns
      * @return {undefined}
      */
-    function loadLists(odffragment, stylesheet) {
+    function loadLists(odffragment, stylesheet, documentns) {
         var i,
             lists,
             node,
@@ -872,7 +874,7 @@ odf.OdfCanvas = (function () {
 
             if (id) {
                 continueList = node.getAttributeNS(textns, "continue-list");
-                node.setAttribute("id", id);
+                node.setAttributeNS(documentns, "id", id);
                 rule = 'text|list#' + id + ' > text|list-item > *:first-child:before {';
 
                 styleName = node.getAttributeNS(textns, 'style-name');
@@ -1157,14 +1159,14 @@ odf.OdfCanvas = (function () {
 
             modifyDrawElements(odfnode.body, css);
             cloneMasterPages(container, shadowContent, odfnode.body, css);
-            modifyTables(odfnode.body);
+            modifyTables(odfnode.body, element.namespaceURI);
             modifyLinks(odfnode.body);
             modifyLineBreakElements(odfnode.body);
             expandSpaceElements(odfnode.body);
             expandTabElements(odfnode.body);
             loadImages(container, odfnode.body, css);
             loadVideos(container, odfnode.body);
-            loadLists(odfnode.body, css);
+            loadLists(odfnode.body, css, element.namespaceURI);
 
             sizer.insertBefore(shadowContent, sizer.firstChild);
             fixContainerSize();

commit 7b1cc1acad8b198bb3a11a3e1c92de98bd1d8147

Remove dead code from OdfCanvas and OdfUtils
The removed code was intended to implement continue list functionality.
However this code no longer worked due to a regression introduced in commit 3ae9e0c.

As I have refactored this area and implemented the continue list
functionality in the new list styles class this old code is no longer
required.

The removed code in OdfCanvas also made use of the
getFirstNonWhitespaceChild function in OdfUtils and the removed code was
the last remaining usage of it. As it is preferable to make use of DOM API
calls which give us the desired behaviour such as
element.firstElementChild I have removed this function.

webodf/lib/odf/OdfCanvas.js
@@ -171,12 +171,9 @@
         /**@const@type {!string}*/tablens = odf.Namespaces.tablens,
         /**@const@type {!string}*/textns  = odf.Namespaces.textns,
         /**@const@type {!string}*/xlinkns = odf.Namespaces.xlinkns,
-        /**@const@type {!string}*/xmlns = odf.Namespaces.xmlns,
         /**@const@type {!string}*/presentationns = odf.Namespaces.presentationns,
         /**@const@type {!string}*/webodfhelperns = "urn:webodf:names:helper",
-        /**@type{?Window}*/window = runtime.getWindow(),
         xpath = xmldom.XPath,
-        odfUtils = new odf.OdfUtils(),
         domUtils = new core.DomUtils();
 
     /**
@@ -707,196 +704,6 @@
     }
 
     /**
-     * COPIED FROM ListStylesToCss - Will be removed when this logic is deduped
-     * Appends the rule into the stylesheets and logs any errors that occur
-     * @param {!CSSStyleSheet} styleSheet
-     * @param {!string} rule
-     */
-    function appendRule(styleSheet, rule) {
-        try {
-            styleSheet.insertRule(rule, styleSheet.cssRules.length);
-        } catch (/**@type{!DOMException}*/e) {
-            runtime.log("cannot load rule: " + rule + " - " + e);
-        }
-    }
-
-    /**
-     * COPIED FROM ListStylesToCss - Will be removed when this logic is deduped
-     * Return the supplied value with any backslashes escaped, and double-quotes escaped
-     * @param {!string} value
-     * @return {!string}
-     */
-    function escapeCSSString(value) {
-        return value.replace(/\\/g, "\\\\").replace(/"/g, "\\\"");
-    }
-
-    /**
-     * @param {!Element} node
-     * @return {!string}
-     */
-    function getNumberRule(node) {
-        var style = node.getAttributeNS(stylens, "num-format"),
-            suffix = node.getAttributeNS(stylens, "num-suffix") || "",
-            prefix = node.getAttributeNS(stylens, "num-prefix") || "",
-            rule = "",
-            /**@type{!Object.<string,string>}*/
-            stylemap = {
-                '1': 'decimal',
-                'a': 'lower-latin',
-                'A': 'upper-latin',
-                'i': 'lower-roman',
-                'I': 'upper-roman'
-            },
-            content;
-
-        if (prefix) {
-            // Content needs to be on a new line if it contains slashes due to a bug in older versions of webkit
-            // E.g., the one used in the qt runtime tests - https://bugs.webkit.org/show_bug.cgi?id=35010
-            content = '"' + escapeCSSString(prefix) + '"\n';
-        }
-
-        if (stylemap.hasOwnProperty(style)) {
-            content += " counter(list, " + stylemap[style] + ")";
-        } else if (style) {
-            content += "'" + style + "';";
-        } else {
-            content += ' ""';
-        }
-        if (suffix) {
-            content += ' "' + escapeCSSString(suffix) + '"';
-        }
-        rule = 'content:' + content + ';';
-        return rule;
-    }
-    /**
-     * @return {!string}
-     */
-    function getImageRule() {
-        var rule = "content: none;";
-        return rule;
-    }
-    /**
-     * @param {!Element} node
-     * @return {!string}
-     */
-    function getBulletRule(node) {
-        var bulletChar = node.getAttributeNS(textns, "bullet-char");
-        // Content needs to be on a new line if it contains slashes due to a bug in older versions of webkit
-        // E.g., the one used in the qt runtime tests - https://bugs.webkit.org/show_bug.cgi?id=35010
-        return 'content: "' + escapeCSSString(bulletChar) + '"\n;';
-    }
-
-    /**
-     * @param {Element|undefined} node
-     * @return {string|undefined}
-     */
-    function getBulletsRule(node) {
-        var itemrule;
-
-        if (node) {
-            if (node.localName === "list-level-style-number") {
-                itemrule = getNumberRule(node);
-            } else if (node.localName === "list-level-style-image") {
-                itemrule = getImageRule();
-            } else if (node.localName === "list-level-style-bullet") {
-                itemrule = getBulletRule(node);
-            }
-        }
-
-        return itemrule;
-    }
-    /**
-     * Load all the lists that are inside an odf element, and correct numbering.
-     * @param {!Element} odffragment
-     * @param {!CSSStyleSheet} stylesheet
-     * @param {!string} documentns
-     * @return {undefined}
-     */
-    function loadLists(odffragment, stylesheet, documentns) {
-        var i,
-            lists,
-            node,
-            id,
-            continueList,
-            styleName,
-            rule,
-            /**@type{!Object.<string,string>}*/
-            listMap = {},
-            parentList,
-            listStyles,
-            /**@type{!Object.<string,!Element>}*/
-            listStyleMap = {},
-            bulletRule;
-
-        listStyles = window.document.getElementsByTagNameNS(textns, "list-style");
-        for (i = 0; i < listStyles.length; i += 1) {
-            node = /**@type{!Element}*/(listStyles.item(i));
-            styleName = node.getAttributeNS(stylens, "name");
-
-            if (styleName) {
-                listStyleMap[styleName] = node;
-            }
-        }
-
-        lists = odffragment.getElementsByTagNameNS(textns, 'list');
-
-        for (i = 0; i < lists.length; i += 1) {
-            node = /**@type{!Element}*/(lists.item(i));
-
-            id = node.getAttributeNS(xmlns, "id");
-
-            if (id) {
-                continueList = node.getAttributeNS(textns, "continue-list");
-                node.setAttributeNS(documentns, "id", id);
-                rule = 'text|list#' + id + ' > text|list-item > *:first-child:before {';
-
-                styleName = node.getAttributeNS(textns, 'style-name');
-                if (styleName) {
-                    node = listStyleMap[styleName];
-                    // TODO: getFirstNonWhitespaceChild() could also return a comment. Ensure the result is proper!
-                    bulletRule = getBulletsRule(/**@type{Element|undefined}*/(odfUtils.getFirstNonWhitespaceChild(node)));
-                    // Content needs to be on a new line if it contains slashes due to a bug in older versions of webkit
-                    // E.g., the one used in the qt runtime tests - https://bugs.webkit.org/show_bug.cgi?id=35010
-                    bulletRule = "\n" + bulletRule + "\n";
-                }
-
-                if (continueList) {
-                    parentList = listMap[continueList];
-                    while (parentList) {
-                        parentList = listMap[parentList];
-                    }
-                    rule += 'counter-increment:' + continueList + ';';
-
-                    if (bulletRule) {
-                        bulletRule = bulletRule.replace('list', continueList);
-                        rule += bulletRule;
-                    } else {
-                        rule += 'content:counter(' + continueList + ');';
-                    }
-                } else {
-                    continueList = "";
-                    if (bulletRule) {
-                        bulletRule = bulletRule.replace('list', id);
-                        rule += bulletRule;
-                    } else {
-                        rule += 'content: counter(' + id + ');';
-                    }
-                    rule += 'counter-increment:' + id + ';';
-                    appendRule(stylesheet, 'text|list#' + id + ' {counter-reset:' + id + '}');
-                }
-                rule += '}';
-
-                listMap[id] = continueList;
-
-                if (rule) {
-                    // Add this stylesheet
-                    appendRule(stylesheet, rule);
-                }
-            }
-        }
-    }
-
-    /**
      * @param {!HTMLHeadElement} head
      * @return {?HTMLStyleElement}
      */
@@ -1230,10 +1037,8 @@
             expandTabElements(odfnode.body);
             loadImages(container, odfnode.body, css);
             loadVideos(container, odfnode.body);
-            loadLists(odfnode.body, css, element.namespaceURI);
 
             sizer.insertBefore(shadowContent, sizer.firstChild);
-
             zoomHelper.setZoomableElement(sizer);
         }
 

webodf/lib/odf/OdfUtils.js
@@ -39,9 +39,6 @@ odf.OdfUtils = function OdfUtils() {
         /**@const
            @type{!string}*/
         xlinkns = odf.Namespaces.xlinkns,
-        /**@const
-           @type{!RegExp}*/
-        whitespaceOnly = /^\s*$/,
         domUtils = new core.DomUtils(),
         // only add odf element namespaces here.
         // Namespaces solely used for attributes are excluded. eg. fo, xlink & xml
@@ -627,20 +624,6 @@ odf.OdfUtils = function OdfUtils() {
         }
         return false;
     };
-
-    /**
-     * Returns the first non-whitespace-only child of a given node
-     * @param {Node|undefined} node
-     * @return {Node|undefined}
-     */
-    function getFirstNonWhitespaceChild(node) {
-        var child = node && node.firstChild;
-        while (child && child.nodeType === Node.TEXT_NODE && whitespaceOnly.test(child.nodeValue)) {
-            child = child.nextSibling;
-        }
-        return child;
-    }
-    this.getFirstNonWhitespaceChild = getFirstNonWhitespaceChild;
 
     /**
      * Returns the length split as value and unit, from an ODF attribute