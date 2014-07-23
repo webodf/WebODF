e6cb30164f13a28ecce58b0eab7688676cb5b3f1
Philip Peitsch <philip.peitsch@gmail.com>
2014-07-23 13:49:28 +1000

    Add TODO around TrivialUndoManager bug

--- webodf/lib/gui/TrivialUndoManager.js
+++ webodf/lib/gui/TrivialUndoManager.js
@@ -387,6 +387,8 @@ gui.TrivialUndoManager = function TrivialUndoManager(defaultRules) {
         completeCurrentUndoState();
         // We just threw away the cursors in the snapshot, so need to recover all these operations so the
         // cursor can be re-inserted when an undo is performed
+        // TODO the last move state may not reflect a valid position in the document!!!
+        // E.g., add cursor, move to end, delete all content + saveInitialState
         currentUndoStateTransition = initialStateTransition = extractCursorStates([initialStateTransition].concat(undoStateTransitions));
         undoStateTransitions.length = 0;
         redoStateTransitions.length = 0;
