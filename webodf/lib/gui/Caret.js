webodf/WebODF

commit f78a30a5051806863c9425b02f3058fb4c1cd693

Fix caret placement when composing text via the IME
If the cursor node itself is visible, use the bounding rectangle from that
as the highest priority for the caret position.

webodf/lib/gui/Caret.js
@@ -164,62 +164,69 @@ gui.Caret = function Caret(cursor, avatarInitiallyVisible, blinkOnRangeSelect) {
             rootRect = /**@type{!ClientRect}*/(domUtils.getBoundingClientRect(canvas.getSizer())),
             useLeftEdge = false;
 
-        // TODO this might be able to use OdfUtils.scanLeft & scanRight behaviours to find the next odf element
-        // By default, assume the selection height should reflect the height of the previousSibling's last client rect
-        // This means if the cursor is next to a text node, the client rect will be the dimensions of the text block
-        if (node.previousSibling) {
-            nodeLength = length(node.previousSibling);
-            range.setStart(node.previousSibling, nodeLength > 0 ? nodeLength - 1 : 0);
-            range.setEnd(node.previousSibling, nodeLength);
-            nextRectangle = range.getBoundingClientRect();
-            if (nextRectangle && nextRectangle.height) {
-                selectionRectangle = nextRectangle;
-            }
-        }
-        // Under some circumstances (either no previous sibling, or whitespace wrapping) the client rect of the next
-        // sibling will actually be a more accurate visual representation.
-        if (node.nextSibling) {
-            range.setStart(node.nextSibling, 0);
-            range.setEnd(node.nextSibling, length(node.nextSibling) > 0 ? 1 : 0);
-            nextRectangle = range.getBoundingClientRect();
-            if (nextRectangle && nextRectangle.height) {
-                // The nextSibling's rectangle should take precedence if
-                // 1. There is no previousSibling
-                // or 2. The nextSibling's rectangle has more vertical overlap with the cursor node's bounding rectangle
-                // Check #2 is specifically required to handling whitespace wrapping logic. Without this check,
-                // when a whitespace block is wrapped, the cursor tends to jump to the vertical alignment of the previous
-                // line, rather than the line the cursor element is now actually on.
-                if (!selectionRectangle || verticalOverlap(node, nextRectangle) > verticalOverlap(node, selectionRectangle)) {
+        if (node.getClientRects().length > 0) {
+            // If the cursor node itself is visible, use that as the caret location.
+            // The most common reason for the cursor to be visible is because the user is entering some text
+            // via an IME
+            selectionRectangle = domUtils.getBoundingClientRect(node);
+        } else {
+            // TODO this might be able to use OdfUtils.scanLeft & scanRight behaviours to find the next odf element
+            // By default, assume the selection height should reflect the height of the previousSibling's last client rect
+            // This means if the cursor is next to a text node, the client rect will be the dimensions of the text block
+            if (node.previousSibling) {
+                nodeLength = length(node.previousSibling);
+                range.setStart(node.previousSibling, nodeLength > 0 ? nodeLength - 1 : 0);
+                range.setEnd(node.previousSibling, nodeLength);
+                nextRectangle = range.getBoundingClientRect();
+                if (nextRectangle && nextRectangle.height) {
                     selectionRectangle = nextRectangle;
+                }
+            }
+            // Under some circumstances (either no previous sibling, or whitespace wrapping) the client rect of the next
+            // sibling will actually be a more accurate visual representation.
+            if (node.nextSibling) {
+                range.setStart(node.nextSibling, 0);
+                range.setEnd(node.nextSibling, length(node.nextSibling) > 0 ? 1 : 0);
+                nextRectangle = range.getBoundingClientRect();
+                if (nextRectangle && nextRectangle.height) {
+                    // The nextSibling's rectangle should take precedence if
+                    // 1. There is no previousSibling
+                    // or 2. The nextSibling's rectangle has more vertical overlap with the cursor node's bounding rectangle
+                    // Check #2 is specifically required to handling whitespace wrapping logic. Without this check,
+                    // when a whitespace block is wrapped, the cursor tends to jump to the vertical alignment of the previous
+                    // line, rather than the line the cursor element is now actually on.
+                    if (!selectionRectangle || verticalOverlap(node, nextRectangle) > verticalOverlap(node, selectionRectangle)) {
+                        selectionRectangle = nextRectangle;
+                        useLeftEdge = true;
+                    }
+                }
+            }
+
+            if (!selectionRectangle) {
+                // Handle the case where a cursor ends up inside an empty paragraph. There are no nearby text elements
+                // to get a rect from, so use the paragraph BCR instead, as it's better than nothing
+                paragraph = odfUtils.getParagraphElement(node);
+                if (paragraph) {
+                    selectionRectangle = domUtils.getBoundingClientRect(paragraph);
                     useLeftEdge = true;
                 }
             }
-        }
 
-        if (!selectionRectangle) {
-            // Handle the case where a cursor ends up inside an empty paragraph. There are no nearby text elements
-            // to get a rect from, so use the paragraph BCR instead, as it's better than nothing
-            paragraph = odfUtils.getParagraphElement(node);
-            if (paragraph) {
-                selectionRectangle = domUtils.getBoundingClientRect(paragraph);
-                useLeftEdge = true;
-            }
-        }
+            if (!selectionRectangle) {
+                // Finally, if there is still no selection rectangle, crawl up the DOM hierarchy the cursor node is in
+                // and try and find something visible to use. Less ideal than actually having a visible rect... better than
+                // crashing or hiding the caret entirely though :)
 
-        if (!selectionRectangle) {
-            // Finally, if there is still no selection rectangle, crawl up the DOM hierarchy the cursor node is in
-            // and try and find something visible to use. Less ideal than actually having a visible rect... better than
-            // crashing or hiding the caret entirely though :)
-            
-            runtime.log("WARN: No suitable client rectangle found for visual caret for " + cursor.getMemberId());
-            // TODO are the better fallbacks than this?
-            while (node) {
-                if (/**@type{!Element}*/(node).getClientRects().length > 0) {
-                    selectionRectangle = domUtils.getBoundingClientRect(node);
-                    useLeftEdge = true;
-                    break;
+                runtime.log("WARN: No suitable client rectangle found for visual caret for " + cursor.getMemberId());
+                // TODO are the better fallbacks than this?
+                while (node) {
+                    if (/**@type{!Element}*/(node).getClientRects().length > 0) {
+                        selectionRectangle = domUtils.getBoundingClientRect(node);
+                        useLeftEdge = true;
+                        break;
+                    }
+                    node = node.parentNode;
                 }
-                node = node.parentNode;
             }
         }
 

webodf/webodf.css
@@ -302,6 +302,8 @@ cursor|cursor {
     height: auto;
     display: block;
     position: absolute;
+    bottom: 0;
+    right: 0;
     width: 1px;
     outline: none;
     opacity: 0;

webodf/lib/gui/CaretManager.js
@@ -83,6 +83,9 @@ gui.CaretManager = function CaretManager(sessionController) {
         if (caret) {
             /*jslint emptyblock:true*/
             caret.destroy(function() {});
+            if (memberId === sessionController.getInputMemberId()) {
+                sessionController.getEventManager().unsubscribe("compositionupdate", caret.handleUpdate);
+            }
             /*jslint emptyblock:false*/
             delete carets[memberId];
         }
@@ -196,6 +199,7 @@ gui.CaretManager = function CaretManager(sessionController) {
 
             // wire up the cursor update to caret visibility update
             cursor.subscribe(ops.OdtCursor.signalCursorUpdated, scheduleCaretVisibilityCheck);
+            eventManager.subscribe("compositionupdate", caret.handleUpdate);
             // Add event trap as an overlay element to the caret
             caret.setOverlayElement(eventManager.getEventTrap());
         } else {

commit 01299999b0cbff5dc1c0d51bd999c7e2eb1becc7

Keep IME character selection menu in one place during composition
f78a30a introduced a regression that caused the IME menu to appear to
follow the caret across the screen as new characters are entered, rather
than remaining anchored at the beginning of the composed text.

This patch adds some width calculations, and aligns parts of the caret
overlay to either left or right side of the container. As the caret
overlay is widened, the event trap will remain fixed to the left, and the
caret + avatar handle will remain fixed to the right.

webodf/lib/gui/Caret.js
@@ -128,7 +128,7 @@ gui.Caret = function Caret(cursor, avatarInitiallyVisible, blinkOnRangeSelect) {
      * position. The horizontal position of the caret is specified in the "right" property
      * as a caret generally appears to the right of the character or object is represents.
      *
-     * @return {!{height: !number, top: !number, right: !number}}
+     * @return {!{height: !number, top: !number, right: !number, width: !number}}
      */
     function getSelectionRect() {
         var node = cursor.getNode(),
@@ -136,7 +136,8 @@ gui.Caret = function Caret(cursor, avatarInitiallyVisible, blinkOnRangeSelect) {
             nextRectangle,
             selectionRectangle,
             rootRect = /**@type{!ClientRect}*/(domUtils.getBoundingClientRect(canvas.getSizer())),
-            useLeftEdge = false;
+            useLeftEdge = false,
+            width = 0;
 
         if (node.getClientRects().length > 0) {
             // If the cursor is visible, use that as the caret location.
@@ -144,6 +145,8 @@ gui.Caret = function Caret(cursor, avatarInitiallyVisible, blinkOnRangeSelect) {
             // via an IME, or no nearby rect was discovered and cursor was forced visible for caret rect calculations
             // (see below when the show-caret attribute is set).
             selectionRectangle = getCaretSizeFromCursor();
+            // The space between the cursor BCR and the caretSizer is the width consumed by any visible composition text
+            width = selectionRectangle.left - domUtils.getBoundingClientRect(node).left;
             useLeftEdge = true;
         } else {
             // Need to resync the stepIterator prior to every use as it isn't automatically kept up-to-date
@@ -193,7 +196,8 @@ gui.Caret = function Caret(cursor, avatarInitiallyVisible, blinkOnRangeSelect) {
         caretRectangle = {
             top: selectionRectangle.top,
             height: selectionRectangle.height,
-            right: useLeftEdge ? selectionRectangle.left : selectionRectangle.right
+            right: useLeftEdge ? selectionRectangle.left : selectionRectangle.right,
+            width: domUtils.adaptRangeDifferenceToZoomLevel(width, canvas.getZoomLevel())
         };
         return caretRectangle;
     }
@@ -224,7 +228,8 @@ gui.Caret = function Caret(cursor, avatarInitiallyVisible, blinkOnRangeSelect) {
         }
         caretOverlay.style.height = selectionRect.height + "px";
         caretOverlay.style.top = selectionRect.top + "px";
-        caretOverlay.style.left = selectionRect.right + "px";
+        caretOverlay.style.left = (selectionRect.right - selectionRect.width) + "px";
+        caretOverlay.style.width = selectionRect.width ? (selectionRect.width + "px") : "";
 
         // Update the overlay element
         if (overlayElement) {

webodf/webodf.css
@@ -263,7 +263,6 @@ cursor|cursor {
     position: absolute;
     top: 5%; /* push down the caret; 0px can do the job, 5% looks better, 10% is a bit over */
     height: 1em;
-    margin-left: -1px;
     z-index: 10;
     pointer-events: none;
 }
@@ -273,6 +272,7 @@ cursor|cursor {
     border-left: 2px solid black;
     top: 0;
     bottom: 0;
+    right: 0;
 }
 
 .webodf-caretOverlay .handle {
@@ -291,7 +291,7 @@ cursor|cursor {
     border: 1px solid black;
 
     top: -85px;
-    left: -32px;
+    right: -32px;
 }
 
 .webodf-caretOverlay .handle > img {
@@ -335,7 +335,7 @@ cursor|cursor {
     display: block;
     position: absolute;
     bottom: 0;
-    right: 0;
+    left: 0;
     width: 1px;
     outline: none;
     opacity: 0;